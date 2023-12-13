647eb611da60c180dbfbcdbb71717a93a3d2c021
sshkey <151507835+poleboinagopi@users.noreply.github.com>
2023-12-13 19:14:50 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -31,7 +31,7 @@ variable "image_id" {
 
 variable "az_name" {
   description = "Provide AZ name as per Region to launch Instance"
-  default = "us-east-2"
+  default = "ap-south-1"
 }
 
 variable "keyname" {
