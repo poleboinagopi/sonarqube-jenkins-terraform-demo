5e7fe619ce9593ea247f1b907d77a9ad81fa174b
sshkey <151507835+poleboinagopi@users.noreply.github.com>
2023-12-13 18:35:58 +0530

    Update instance.tf

--- instance.tf
+++ instance.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "web" {
   ami                         = var.image_id     #"ami-09d56f8956ab235b3"
-  instance_type               = var.inst_type[1] // or var.inst_type[0], var.inst_type[2]
+  instance_type               = var.inst_type[0] // or var.inst_type[0], var.inst_type[2]
   count                       = var.inst_count
   associate_public_ip_address = false
   availability_zone           = var.az_name
